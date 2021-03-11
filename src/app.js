fd5a58cafabeb482ce77cbd47c75702b22e6e6ec
MinJunSeo <smj0385@naver.com>
2021-03-11 13:47:11 +0900

    fix: make directory command handler

--- src/app.js
+++ src/app.js
@@ -5,18 +5,12 @@ program
   .version("0.0.1", "-v, --version");
 
 program
-  .option("-n=dir, --new=dir [path]", "Make new directory");
+  .option("-n=dir, --new=dir <path>", "Make new directory");
 
 program.parse();
 
 const options = program.opts();
 
 if (options["new=dir"]) {
-  if (options["new=dir"] !== true) {
-    // call makeDir functions with [path] value
-    makeDir(options["new=dir"]);
-  } else {
-    console.error("Please enter [path]");
-    program.help();
-  }
+  makeDir(options["new=dir"]);
 }
