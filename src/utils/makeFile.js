84ae1724a54aab34f871331bf574ef60c8b81d9a
MinJunSeo <smj0385@naver.com>
2021-03-11 14:53:28 +0900

    fix: dirname

--- src/utils/makeFile.js
+++ src/utils/makeFile.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 
 const makeFile = (filename) => {
-  const name = __dirname + '/' + path.basename(filename);
+  const name = path.dirname(filename) + '/' + path.basename(filename);
 
   if (fs.existsSync(name)) {
     return console.error(`${path.basename(name)} file is already creaetd`);
