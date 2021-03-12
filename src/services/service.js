d26d0f335d6d12dc5ca501d49e21821772a36467
MinJunSeo <smj0385@naver.com>
2021-03-12 09:06:28 +0900

    feat: classify directory string and file name string

--- src/services/service.js
+++ src/services/service.js
@@ -2,3 +2,11 @@ const { readFile } = require("../utils");
 
 let content = readFile(__dirname + "/sample.txt");
 content = content.split("\r\n");
+
+for (const name of content) {
+  if (name.indexOf('/') !== -1) {
+    console.log(`${name} is directory`);
+  } else {
+    console.log(`${name} is file.`);
+  }
+}
