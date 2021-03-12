4224e6b57dbc29341a2c9a1866fe49584bfb8e02
MinJunSeo <smj0385@naver.com>
2021-03-12 09:50:18 +0900

    feat: get node's parent

--- src/services/service.js
+++ src/services/service.js
@@ -1,12 +1,25 @@
 const { readFile } = require("../utils");
+const { Tree } = require("../dataStructure");
+const path = require("path");
 
 let content = readFile(__dirname + "/sample.txt");
 content = content.split("\r\n");
 
-for (const name of content) {
-  if (name.indexOf('/') !== -1) {
-    console.log(`${name} is directory`);
-  } else {
-    console.log(`${name} is file.`);
+const rootDir = path.resolve(__dirname, "../../");
+
+const getParent = (index, name) => {
+  const indention = name.split(' ').length - 1;
+
+  if (!indention) {
+    return rootDir;
   }
-}
+
+  while (index - 1 >= 0) {
+    if (content[index - 1].split(' ').length - 1 === indention - 2) {
+      return content[index - 1];
+    }
+    index -= 1;
+  }
+
+  throw new Error("규칙에 올바르게 작성되지 않은 파일입니다.");
+};
