6f29b155fce3fcc1e8b5faa1b1041f7d2e5455f3
MinJunSeo <smj0385@naver.com>
2021-03-12 09:59:36 +0900

    fix: content init

--- src/services/service.js
+++ src/services/service.js
@@ -2,10 +2,15 @@ const { readFile } = require("../utils");
 const { Tree } = require("../dataStructure");
 const path = require("path");
 
-let content = readFile(__dirname + "/sample.txt");
-content = content.split("\r\n");
-
 const rootDir = path.resolve(__dirname, "../../");
+
+const content = [];
+for (const data of readFile(__dirname + "/sample.txt").split("\r\n")) {
+  content.push({
+    indention: data.split(' ').length - 1,
+    name: data.replace(/ /g, '')
+  });
+}
 
 const getParent = (index, name) => {
   const indention = name.split(' ').length - 1;
@@ -16,7 +21,7 @@ const getParent = (index, name) => {
 
   while (index - 1 >= 0) {
     if (content[index - 1].split(' ').length - 1 === indention - 2) {
-      return content[index - 1];
+      return content[index - 1].replace(/ /g, '');
     }
     index -= 1;
   }
