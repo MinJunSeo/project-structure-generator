fdfa251fdda97291a002378e1d677a0c8f4a3d10
MinJunSeo <smj0385@naver.com>
2021-03-19 09:10:55 +0900

    feat: service init command

--- src/services/program.js
+++ src/services/program.js
@@ -1,7 +1,26 @@
 const program = require("commander");
 
+const { AlreadyServiceInit } = require("../exceptions");
+const { makeDir, makeFile } = require("../utils");
+
+const ProjectStructureGeneratorService = require("./projectStructGenerator");
+let projectStructureGeneratorService = null;
+
 program
   .usage("<command> [option]")
   .version("1.0.0", "-v, --version")
+
+program
+  .command("init")
+  .description("Project Structure Generator service init.")
+  .action(() => {
+    if (projectStructureGeneratorService) {
+      throw AlreadyServiceInit;
+    }
+
+    projectStructureGeneratorService = new ProjectStructureGeneratorService(process.cwd());
+    makeDir(process.cwd() + "/.projectStructure");
+    makeFile(process.cwd() + "/.projectStructure/README.TXT");
+  });
 
 module.exports = program;
