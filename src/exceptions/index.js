66b4bfde8238c9466782690e7720a2f04ec5c4e4
MinJunSeo <smj0385@naver.com>
2021-03-19 09:54:32 +0900

    feat: not init service exception

--- src/exceptions/index.js
+++ src/exceptions/index.js
@@ -1,2 +1,3 @@
 exports.NotAlrightFileForm = new Error("Given file that not correct to alrigh file form.");
 exports.AlreadyServiceInit = new Error("Project Structure Service already init.");
+exports.NotInitService = new Error("Please enter init command before using this service");
