ff7011c60e26277d35e9ee0f74031e7a82269452
MinJunSeo <smj0385@naver.com>
2021-03-22 19:06:08 +0900

    feat: print message when make dir successfully

--- src/utils/makeDir.js
+++ src/utils/makeDir.js
@@ -1,12 +1,15 @@
-const { mkdirSync } = require("fs");
+const fs = require("fs");
 const { DirectoryIsExists } = require("../exceptions");
 const isExists = require("./isExists");
+const printSuccessMessage = require("./printSuccessMessage");
 
 const makeDir = (dirPath) => {
   if (isExists(dirPath)) {
     throw DirectoryIsExists;
   }
-  mkdirSync(dirPath);
+
+  fs.mkdirSync(dirPath);
+  printSuccessMessage("Success: make directory");
 };
 
 module.exports = makeDir;
