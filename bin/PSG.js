29480e5eaaec982a9a074d894ae87d2737610831
MinJunSeo <smj0385@naver.com>
2021-03-31 16:10:50 +0900

    fix: always print "Command Not Found" err

--- bin/PSG.js
+++ bin/PSG.js
@@ -8,7 +8,7 @@ psgApp
   .version("1.0.0", "-v, --version");
 
 psgApp
-  .action(printCommandNotFound(psgApp));
+  .action(() => printCommandNotFound(psgApp));
 
 try {
   psgApp.parse();
--- bin/touch.js
+++ bin/touch.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 
-const { touchApp, psgApp } = require("../lib/src/services");
+const { touchApp } = require("../lib/src/services");
 const { printCommandNotFound, printExceptionMessage } = require("../lib/src/utils");
 
 touchApp
   .version("1.0.0", "-v, --version");
 
 touchApp
-  .action(printCommandNotFound(touchApp));
+  .action(() => printCommandNotFound(touchApp));
 
 try {
   touchApp.parse();
