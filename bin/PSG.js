1a07e0ea3279518931f86c199c57398c06504f36
MinJunSeo <smj0385@naver.com>
2021-03-31 16:03:25 +0900

    refactor: use util

--- bin/PSG.js
+++ bin/PSG.js
@@ -1,17 +1,14 @@
 #!/usr/bin/env node
 
 const { psgApp } = require("../lib/src/services")
-const { printExceptionMessage } = require("../lib/src/utils");
+const { printCommandNotFound, printExceptionMessage } = require("../lib/src/utils");
 
 psgApp
   .usage("command")
   .version("1.0.0", "-v, --version");
 
 psgApp
-  .action(() => {
-    printExceptionMessage("Error: Command not found");
-    psgApp.help();
-  });
+  .action(printCommandNotFound(psgApp));
 
 try {
   psgApp.parse();
