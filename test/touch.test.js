eea2b4e06227a59bf67d92a690adb5f541b5358a
MinJunSeo <smj0385@naver.com>
2021-05-07 09:04:37 +0900

    docs: make a.txt an empty file

--- test/touch.test.js
+++ test/touch.test.js
@@ -2,19 +2,19 @@ import { touchApp } from "../lib/src/services";
 import fs from "fs";
 
 describe("touch - make an empty file", () => {
-  test("make 'a.txt' at here", async () => {
-    const myArguments = process.argv;
-    myArguments.push(__dirname + "/a.txt");
-
-    await touchApp.parse(myArguments, { from: "user" });
-    expect(fs.existsSync(__dirname + "./a.txt"));
-  });
-
-  afterAll(() => {
-    fs.unlink(__dirname + "/a.txt", error => {
-      if (error) {
-        console.error(error);
+  describe("filename === 'a.txt'", () => {
+    afterAll(() => {
+      if (fs.existsSync(__dirname + "/a.txt")) {
+        fs.unlinkSync(__dirname + "/a.txt");
       }
+    });
+
+    test("make 'a.txt' at here", async () => {
+      const myArguments = process.argv;
+      myArguments.push(__dirname + "/a.txt");
+  
+      await touchApp.parse(myArguments, { from: "user" });
+      expect(fs.existsSync(__dirname + "/a.txt")).toBeTruthy();
     });
   });
 });
